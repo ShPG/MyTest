6ba18c82835b121b28207919fd0cc797f2938438
spg <597382457@qq.com>
2018-06-25 18:06:44 +0800

    Initial commit

---
+++ app/src/main/java/com/myexample/sipg/huidiao/MainActivity.java
@@ -0,0 +1,34 @@
+package com.myexample.sipg.huidiao;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View btn=findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri=Uri.parse("bocmcht://payresult/mobile?jsonData=111");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                startActivityForResult(intent,0);
+            }
+        });
+
+
+
+
+
+
+
+    }
+}
